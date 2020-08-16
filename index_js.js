aa7357866c81f79a362c6c5fcf551e344614c71b
Aasiya-8833 <56215830+Aasiya-8833@users.noreply.github.com>
2020-08-16 23:33:37 +0530

    update

--- index_js.js
+++ index_js.js
@@ -1,80 +1,33 @@
-document.getElementById("after-submit").hidden = true;
-document.getElementById("try-btn").hidden = true;
+$(document).ready(function() {
+    $(".expand-icon").click(function() {
+        $(".side-bar").toggleClass("collapse");
+        $(".main-container").toggleClass("expand-container");
+        if ($(".fas").hasClass("fa-arrow-right")) {
+            $(".fas").addClass('fa-arrow-left').removeClass('fa-arrow-right');
+            return;
+        }
+        if ($(".fas").hasClass("fa-arrow-left")) {
+            $(".fas").addClass('fa-arrow-right').removeClass('fa-arrow-left');
+        }
 
+    });
+    // When the user scrolls the page, execute myFunction
+    window.onscroll = function() {
+        myFunction();
+    };
 
+    // Get the navbar
+    var navbar = document.getElementById("nav-bar");
 
-var checked = [];
-$('input').change(function() {
-    if (this.checked) {
-        var response = $('label[for="' + this.id + '"]');
-        //console.log(response);
-        checked.push(response);
+    // Get the offset position of the navbar
+    var sticky = navbar.offsetTop;
+
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+        }
     }
 });
-
-
-function check() {
-    var count = 0;
-    var q1 = document.quiz.ques1.value;
-    var q2 = document.quiz.ques2.value;
-    var q3 = document.quiz.ques3.value;
-    var q4 = document.quiz.ques4.value;
-    var q5 = document.quiz.ques5.value;
-    if (q1 == "" || q2 == "" || q3 == "" || q4 == "" || q5 == "") {
-        alert("Attempt all questions");
-        return
-    }
-
-
-    // console.log(document.quiz.ques1.value);
-    if (q1 == "_") {
-        count++;
-    }
-
-    if (q2 == "3") {
-        count++;
-    }
-    if (q3 == "Only same data type") {
-        count++;
-    }
-    if (q4 == "Compile time error") {
-        count++;
-    }
-    if (q5 == "Right to Left") {
-        count++;
-    }
-
-
-    var element1 = document.getElementById("a1");
-    element1.classList.add("green");
-    var element2 = document.getElementById("a2");
-    element2.classList.add("green");
-
-    var element3 = document.getElementById("a3");
-    element3.classList.add("green");
-    var element4 = document.getElementById("a4");
-    element4.classList.add("green");
-    var element5 = document.getElementById("a5");
-    element5.classList.add("green");
-
-
-    //console.log(checked);
-    for (var i = 0; i < checked.length; i++) {
-        console.log(checked[i].addClass("red"));
-    }
-
-
-    if (count === 5) {
-        //alert("uytcuycutxs");
-        document.getElementById("number-correct").innerHTML = "Your score = " + count + "   Conguratulations !";
-        document.getElementById("after-submit").hidden = false;
-        document.getElementById("try-btn").hidden = true;
-    } else {
-        document.getElementById("number-correct").innerHTML = "Your score = " + count + "   Keep It Up !";
-        document.getElementById("after-submit").hidden = false;
-        document.getElementById("try-btn").hidden = false;
-    }
-
-    window.scrollTo(0, 500)
-
-}
